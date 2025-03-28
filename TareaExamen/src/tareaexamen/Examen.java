c72f7d829eedc6d774988a1acde495a437936025
S82833 <simonharth61@gmail.com>
2025-03-28 00:17:15 -0500

    .

--- TareaExamen/src/tareaexamen/Examen.java
+++ TareaExamen/src/tareaexamen/Examen.java
@@ -12,6 +12,7 @@ package tareaexamen;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Examen {    
     //Factory para crear objetos pregunta
@@ -21,14 +22,43 @@ public class Examen {
     //Una lista para almacenar las preguntas aleatorias que vamos a instanciar
     private List<Pregunta> preguntas = new ArrayList<>();
     private List<String> respuestas = new ArrayList<>();
-    int calificacion = 0;
+    private int calificacion = 0;
+    List<Integer> numUsados = new ArrayList<>();
+
     
     public Examen(int cantidad){
         for (int i = 0; i < cantidad; i++) {
             int tipoAleatorio = generarAleatorio(0, 2);
+            while(numUsados.contains(tipoAleatorio)){
+                tipoAleatorio = generarAleatorio(0, 2);
+            }
             //Todas las listas tienen el mismo tamaño, son las reglas del negocio
             int preguntaAleatoria = generarAleatorio(0, Marcar.listaEnunciadosM.size()-1);
             this.preguntas.add(preguntaFactory.generarPregunta(tipoAleatorio, preguntaAleatoria));
+            switch (tipoAleatorio) {
+            case 0: // Marcar
+                Marcar.listaEnunciadosM.remove(preguntaAleatoria);
+                Marcar.listaOpcionesM.remove(preguntaAleatoria);
+                Marcar.listaRespuestasM.remove(preguntaAleatoria);
+                if(Marcar.listaEnunciadosM.isEmpty()){
+                    this.numUsados.add(0);
+                }
+                break;
+            case 1: // Completar
+                Completar.listaEnunciadosC.remove(preguntaAleatoria);
+                Completar.listaRespuestasC.remove(preguntaAleatoria);
+                if(Completar.listaEnunciadosC.isEmpty()){
+                    this.numUsados.add(1);
+                }
+                break;
+            case 2: // Verdadero / Falso
+                VerdaderoFalso.listaEnunciadosVF.remove(preguntaAleatoria);
+                VerdaderoFalso.listaRespuestasVF.remove(preguntaAleatoria);
+                if(VerdaderoFalso.listaEnunciadosVF.isEmpty()){
+                    this.numUsados.add(2);
+                }
+                break;
+            }   
         }
     }
     
@@ -45,6 +75,7 @@ public class Examen {
             Pregunta pregunta = preguntas.get(i);
             String respuesta = respuestas.get(i);
             boolean x = pregunta.esCorrecta(respuesta);
+            System.out.println(x);
             if (x) {
                 this.calificacion += 2;
             }
